99960d8785f54d581f79b7f364814a5a7866a744
Samuel Coizet <messa57fr@gmail.com>
2022-07-07 22:33:29 +0200

    cloudinary and some design done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,6 +48,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :url, :user)
+    params.require(:post).permit(:title, :content, :url, :user, :photo)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :url, presence: true
+  has_one_attached :photo
 end
