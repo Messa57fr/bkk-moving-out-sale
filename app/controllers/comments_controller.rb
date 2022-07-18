f097ea0ea612a5ea372e49abcb6dbe75330bc4fd
Samuel Coizet <messa57fr@gmail.com>
2022-07-19 01:46:06 +0700

    added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,19 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.post = @post
-    @comment.user = current_user
+    @comment.user = User.last
     if @comment.save
       redirect_to post_path(@comment.post), notice: 'Your comment has been added'
     else
       render "posts/show", warning: 'Your comment has not been added'
     end
+    console
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :user, :post)
+    params.require(:comment).permit(:content, :post, :name)
   end
 
 end
